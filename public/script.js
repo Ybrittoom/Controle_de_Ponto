7cbd32202b4d806a9f50375f1ea5624ba4d29033
Ybrittoom <yagoemanuel99@gmail.com>
2025-07-08 13:07:04 -0300

    ..

---
+++ public/script.js
@@ -0,0 +1,15 @@
+//funçao para abri o modal
+function abrir_modal(idModal) {
+    const modal = document.getElementById(idModal)
+    if (modal) {
+        modal.style.display = "block"
+    }
+}
+
+//fechar modal
+function fechar_modal(idModal) {
+    const modal = document.getElementById(idModal)
+    if (modal) {
+        modal.style.display = "none"
+    }
+}
